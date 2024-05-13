c3c7cd976fa8050501a5dcec13dc8e8044de9de6
gurpreet <gss331729@gmail.com>
2024-05-13 09:26:11 +0530

    live conniction

--- app.py
+++ app.py
@@ -258,15 +258,12 @@ def gss():
       title =  re.sub("[!@#$%^&*...']", " ", titles)
 
       mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="mydatabase"
+        
 
-        # host="localhost",
-        # user="egxcmgbg_brandonly",
-        # password="brandonly!@#",
-        # database="egxcmgbg_ecomrce_website"
+        host="localhost",
+        user="egxcmgbg_brandonly",
+        password="brandonly!@#",
+        database="egxcmgbg_ecomrce_website"
       )
 
       mycursor = mydb.cursor()
